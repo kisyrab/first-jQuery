4f407c70b52443993d796cc64294c801bf210afe
kisyrab <906ksy@naver.com>
2024-07-01 14:06:35 +0900

    first jQuery

---
+++ js/index5.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    $(".btn1").click(function(){
+        $("body").animate({"background-color" : "#3AA6B9"} , 1000);
+
+
+    }) /* .btn1 */
+
+    $(".btn2").click(function(){
+        $("body").animate({"background-color" : "#FFD0D0"} , 1000);
+
+
+    }) /* .btn2 */
+
+    $(".btn3").click(function(){
+        $("body").animate({"background-color" : "#FF9EAA"} , 1000);
+
+
+    }) /* .btn3 */
+
+    $(".btn4").click(function(){
+        $("body").animate({"background-color" : "#F9F9E0"} , 1000);
+
+
+    }) /* .btn1 */
+
+
+
+}) /* j */
---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+    $(".btn").click(function(){
+        // $(".img_box").css({"width" : "100%"});
+        // $(".img_box").fadeToggle();
+
+        $(".img_box").animate({"width" : "100%" , "opacity" : "1"} , 1000)
+
+
+
+
+
+
+
+    }) /* btn click */
+}) /* jq */
